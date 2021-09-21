b6676ddf4bd86c92baf043725d4da8821053670a
parsa <parsamesg@gmail.com>
2021-09-21 21:15:59 +0100

    add correct methods for unit tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,21 @@
 require "./lib/bike"
 
 class DockingStation
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    Bike.new
+    @bike = Bike.new
+    @bike
+  end
+
+  def dock_bike(bike)
+    @bikes << bike
+    "Your bike has successfully been docked."
+  end
+
+  def show_bikes
+    @bikes
   end
 end
